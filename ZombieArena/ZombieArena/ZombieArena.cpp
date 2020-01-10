c6b1825be262a00c785f5a59037b9b0066160fe6
ScarletBird <fgfogolin@hotmail.com>
2020-01-10 14:48:18 -0200

    added zombies

--- ZombieArena/ZombieArena/ZombieArena.cpp
+++ ZombieArena/ZombieArena/ZombieArena.cpp
@@ -1,16 +1,20 @@
 #include <SFML/Graphics.hpp>
-
 #include "ZombieArena.h"
 #include "Player.h"
+#include "TextureHolder.h"
 
 using namespace sf;
 
 int main()
 {
+	// Here is the instance of TextureHolder
+	TextureHolder holder;
+
 	// The game will always be in one of four states
 	enum class State { PAUSED, LEVELING_UP, GAME_OVER, PLAYING };
 	// Start with the GAME_OVER state
 	State state = State::GAME_OVER;
+
 
 	// Get the screen resolution and create an SFML window
 	Vector2f resolution;
@@ -42,8 +46,13 @@ int main()
 	// Create the background
 	VertexArray background;
 	// Load the texture for our background vertex array
-	Texture textureBackground;
-	textureBackground.loadFromFile("graphics/background_sheet.png");
+	Texture textureBackground = TextureHolder::GetTexture(
+		"graphics/background_sheet.png");
+
+	// Prepare for a horde of zombies
+	int numZombies;
+	int numZombiesAlive;
+	Zombie* zombies = NULL;
 
 	// The main game loop
 	while (window.isOpen())
@@ -189,6 +198,14 @@ int main()
 				// Spawn the player in the middle of the arena
 				player.spawn(arena, resolution, tileSize);
 
+				// Create a horde of zombies
+				numZombies = 10;
+
+				// Delete the previously allocated memory (if it exists)
+				delete[] zombies;
+				zombies = createHorde(numZombies, arena);
+				numZombiesAlive = numZombies;
+
 				// Reset the clock so there isn't a frame jump
 				clock.restart();
 			}
@@ -223,6 +240,16 @@ int main()
 
 			// Make the view centre around the player				
 			mainView.setCenter(player.getCenter());
+
+			// Loop through each Zombie and update them
+			for (int i = 0; i < numZombies; i++)
+			{
+				if (zombies[i].isAlive())
+				{
+					zombies[i].update(dt.asSeconds(), playerPosition);
+				}
+			}
+
 		}// End updating the scene
 
 		 /*
@@ -241,6 +268,12 @@ int main()
 
 			// Draw the background
 			window.draw(background, &textureBackground);
+
+			// Draw the zombies
+			for (int i = 0; i < numZombies; i++)
+			{
+				window.draw(zombies[i].getSprite());
+			}
 
 			// Draw the player
 			window.draw(player.getSprite());
--- ZombieArena/ZombieArena/ZombieArena.h
+++ ZombieArena/ZombieArena/ZombieArena.h
@@ -1,3 +1,7 @@
 #pragma once
+#include "Zombie.h"
+
 using namespace sf;
+
 int createBackground(VertexArray& rVa, IntRect arena);
+Zombie* createHorde(int numZombies, IntRect arena);
